7d72ba49eaf7f54bd4a3df94ccfbea051ec8e38f
tomay <78463932+tomas0178@users.noreply.github.com>
2022-06-23 17:21:45 +0900

    initial commit

---
+++ js/toma.js
@@ -0,0 +1,30 @@
+alert("Hello! world");
+
+$(".more").click(function () {
+    window.location.href = "http://www.w3schools.com";
+})
+$(".overlap-group2").click(function () {
+    window.location.href = "http://www.w3schools.com";
+})
+$(".overlap-group3").click(function () {
+    window.location.href = "http://www.w3schools.com";
+})
+$(".component-1-1").click(function () {
+    window.location.href = "http://www.w3schools.com";
+})
+$(".component-2-1").click(function () {
+    window.location.href = "http://www.w3schools.com";
+})
+$(".component-3-1").click(function () {
+    window.location.href = "http://www.w3schools.com";
+})
+
+
+/*cursor*/
+$('.more').css('cursor', 'pointer');
+$('.overlap-group1').css('cursor', 'pointer');
+$('.overlap-group2').css('cursor', 'pointer');
+$('.overlap-group3').css('cursor', 'pointer');
+$('.component-1-1').css('cursor', 'pointer');
+$('.component-2-1').css('cursor', 'pointer');
+$('.component-3-1').css('cursor', 'pointer');
